40f4f2f6ba008b1160592f9be1746ec9005dccf6
Obafemi Lawal <swissbobo@gmail.com>
2024-09-07 11:11:45 +0300

    finished chpt 1 part I, understood all what was explained except the Sales_item.h file which will be explained later.

--- c++_primer_5th_ed/sales.cpp
+++ c++_primer_5th_ed/sales.cpp
@@ -2,11 +2,18 @@
 #include "Sales_item.h"
 int main()
 {
-    Sales_item book, book2;
     // read ISBN, number of copies sold, and sales price
-    std::cin >> book >> book2;
+    Sales_item book, book2;
     // write ISBN, number of copies sold, total revenue, and average price
-    std::cout << book + book2 << std::endl;
+    std::cin >> book >> book2;
+    if(book.isbn() == book2.isbn()) {
+        std::cout << book + book2 << std::endl;
+        return 0;
+        
+    }
+    else {
+        std::cerr << "Data must refer to same ISBN" << std::endl;
+    }
 
     return 0;
 }
