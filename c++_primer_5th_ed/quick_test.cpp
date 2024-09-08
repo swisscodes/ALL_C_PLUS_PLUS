ae7b05f37d3ee4070b6a9a84aa84c5079057edb0
Obafemi Lawal <swissbobo@gmail.com>
2024-09-08 23:54:32 +0300

    more c++

---
+++ c++_primer_5th_ed/quick_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+    int a = 0;
+     std::cout << ">" << "2M\n" << "<" << std::endl;
+     std::cout << ">" << "2\tM\n" << "<" << std::endl;
+    std::cout << a << std::endl;
+    std::string hello = "hello";
+    std::cout << hello << std::endl;
+}
