41620e054a9979099c9bc1b77cac069f07206ffc
Obafemi Lawal <swissbobo@gmail.com>
2024-09-30 12:53:39 +0300

    c++ getting more interesting and sametime difficult lol but am enjoying.. did the string sort for learning purposes using array pointers

--- c++_primer_5th_ed/array1_pointer.cpp
+++ c++_primer_5th_ed/array1_pointer.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
     std::string str_arr[] = {"zyxwvutsrqponmlkjihgfedcba", "Hello", "world"};
-    
-    auto *ptr = str_arr;
-    std::cout << *++ptr << std::endl;
 
+    // auto *str_ptr = str_arr;
 
+    for (auto &word : str_arr)
+    {
+        // std::transform(word.begin(), word.end(), word.begin(), ::tolower); // converts word to lower
+        size_t count = 0;
+        while (count < word.size())
+        {
+            for (auto abc = begin(word), cde = abc + 1; cde != end(word); ++abc, ++cde)
+            {
+                *abc = tolower(*abc);
+                *cde = tolower(*cde);
+                if (*abc > *cde)
+                {
+                    auto temp = *cde;
+                    *cde = *abc;
+                    *abc = temp;
+                }
+            }
+            ++count;
+        }
+    }
+
+    for (auto word : str_arr)
+    {
+        std::cout << word << std::endl;
+    }
+
+    auto abc = begin(str_arr);
 }
---
+++ c++_primer_5th_ed/vector_from_int_array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int arr[10];
+    std::vector<int> int_vector(begin(arr), end(arr));
+}
