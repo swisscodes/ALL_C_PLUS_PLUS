e89948896356b6cf31656df6f2fa1432a8f6d3ad
Obafemi Lawal <swissbobo@gmail.com>
2024-10-04 17:42:44 +0300

    on functions

---
+++ c++_primer_5th_ed/exceptions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    try {
+        int a = 10;
+    }
+    catch(std::runtime_error err) {
+        ;
+    }
+
+    return 0;
+
+}
---
+++ c++_primer_5th_ed/functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int fact(int val)
+{
+    int result = 1;
+    while (val > 1)
+    {
+        result *= val--;
+    }
+
+    return result;
+}
+
+void ptr_test(int *p)
+{
+    std::cout << *p << std::endl;
+    p = 0;
+    std::cout << p << std::endl;
+
+}
+
+int main()
+{
+    int ptr_num = 55;
+    std::cout << fact(5) << std::endl;
+    auto f = []()
+    {
+        return 42;
+    };
+
+    ptr_test(&ptr_num);
+
+    return 0;
+}
