fbabc1c08c36848ab19d53c4577eaf5ce4a39ebe
Obafemi Lawal <swissbobo@gmail.com>
2024-10-19 09:46:27 +0300

    works from main to be merged to winwods

--- c++_primer_5th_ed/header/revised_sales_data.h
+++ c++_primer_5th_ed/header/revised_sales_data.h
@@ -11,19 +11,23 @@ class RSales_data
         std::string bookNo;
         unsigned units_sold = 0;
         double revenue = 0.0;
+        static inline int aa = 0;  // or you an choose to define it in .cpp file our outside the class as int Rsales_data::aa = 9; without the inline
+        static int ab;  // or you an choose to define it in .cpp file our outside the class as int Rsales_data::ab = 9;
+        //method is also private
+        double avg_price() const { return units_sold? revenue / units_sold : 0;}
 
     // constructors added
     public:
-        RSales_data() = default;
-        RSales_data(const std::string s = ""): bookNo(s) { }
+        // nondelegating constructor initializes members from corresponding arguments
         RSales_data(const std::string s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p*n) { }
-        RSales_data(std::istream &);
+
+        // remaining constructors all delegate to another constructor
+        RSales_data(): RSales_data("", 0, 0) {}
+        explicit RSales_data(std::string s): RSales_data(s, 0,0) {}
+        explicit RSales_data(std::istream &is): RSales_data() { read(is, *this); }
         // new members: operations on Sales_data objects
         std::string isbn() const { return bookNo; }
         RSales_data &combine(const RSales_data &);
-
-    private:
-        double avg_price() const { return units_sold? revenue / units_sold : 0;}
 
     // nonmember Sales_data friend functions
     friend RSales_data add(const RSales_data &, const RSales_data &);
@@ -31,6 +35,8 @@ class RSales_data
     friend std::istream &read(std::istream &, RSales_data &);
     
 };
+
+int RSales_data::ab = 9;
 
 // nonmember Sales_data interface functions
 RSales_data add(const RSales_data &, const RSales_data &);
--- c++_primer_5th_ed/test.cpp
+++ c++_primer_5th_ed/test.cpp
@@ -4,7 +4,6 @@ int a;
 
 int main()
 {
-    int b;
-    std::cout << a << " " << b << std::endl;
-    // always treat unitialiazed as UB/undefined ub = Undefined Behaviour.
+    int a = 5;
+    double daily_tbl[a];
 }
